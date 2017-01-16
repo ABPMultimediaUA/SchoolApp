6d3c916ee7ec70fae1daa8d8c3504d6f3fc8b48b
Nahiara Latorre Gómez <Nahiara Latorre Gmez>
2017-01-16 20:23:02 +0100

    Transformacion de formatos HECHO!

---
+++ Desarrollo/SDM/MemoriaABP 16Ene/Transf_Formatos/prueba/alumnos.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 16-01-2017 a las 20:01:02
+-- Versión del servidor: 10.1.19-MariaDB
+-- Versión de PHP: 7.0.13
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `pruebabd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumnos`
+--
+
+CREATE TABLE `alumnos` (
+  `id` int(11) NOT NULL,
+  `Nombre` varchar(50) NOT NULL DEFAULT '0',
+  `Apellido` varchar(50) NOT NULL DEFAULT '0',
+  `Sexo` tinyint(4) NOT NULL DEFAULT '0',
+  `FechaNacimiento` varchar(20) DEFAULT NULL,
+  `FechaRegistro` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `alumnos`
+--
+
+INSERT INTO `alumnos` (`id`, `Nombre`, `Apellido`, `Sexo`, `FechaNacimiento`, `FechaRegistro`) VALUES
+(1, 'Antonio ', 'Martinez', 1, '1995-02-12', '2014-05-26'),
+(3, 'Lucia', 'Rodriguez Gonzalez', 2, '1985-04-11', '2014-05-26'),
+(4, 'Pedro', 'Suarez Gomez', 1, '1991-08-17', '2014-05-26'),
+(5, 'Raul', 'Perez Torres', 1, '1989-03-15', '2014-05-26');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
